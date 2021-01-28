66603dd0f4461ed8753734ab6eada24e3b893e56
Fact S Musingarimi <fmusingarimi@gmail.com>
2021-01-28 11:46:44 +0200

    Use CardsReader to read user input from in PokerGameApplication

--- src/main/java/com/inclusivitysolutions/codingassignment/pokergame/PokerGameApplication.java
+++ src/main/java/com/inclusivitysolutions/codingassignment/pokergame/PokerGameApplication.java
@@ -1,13 +1,29 @@
 package com.inclusivitysolutions.codingassignment.pokergame;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Set;
+
 @SpringBootApplication
-public class PokerGameApplication {
+public class PokerGameApplication implements CommandLineRunner {
+    private static final Logger LOGGER = LoggerFactory.getLogger(PokerGameApplication.class);
+    public final CardsReader cardsReader;
 
-	public static void main(String[] args) {
-		SpringApplication.run(PokerGameApplication.class, args);
-	}
+    public PokerGameApplication(CardsReader cardsReader) {
+        this.cardsReader = cardsReader;
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(PokerGameApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        LOGGER.info("Paying poker game");
+        Set<Card> cards = cardsReader.readCardSelection();
+    }
 }
