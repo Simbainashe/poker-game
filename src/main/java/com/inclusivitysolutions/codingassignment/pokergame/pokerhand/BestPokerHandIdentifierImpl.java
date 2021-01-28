136713856b7c10f439b21dbdc0a4c200637f6570
Fact S Musingarimi <fmusingarimi@gmail.com>
2021-01-28 12:57:16 +0200

    Implement BestPokerHandIdentifierImpl

---
+++ src/main/java/com/inclusivitysolutions/codingassignment/pokergame/pokerhand/BestPokerHandIdentifierImpl.java
@@ -0,0 +1,30 @@
+package com.inclusivitysolutions.codingassignment.pokergame.pokerhand;
+
+import com.inclusivitysolutions.codingassignment.pokergame.card.Card;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * @author fact
+ * 28/1/2021
+ */
+@Service
+class BestPokerHandIdentifierImpl implements BestPokerHandIdentifier {
+    private static final Logger LOGGER = LoggerFactory.getLogger(BestPokerHandIdentifierImpl.class);
+    private static final List<PokerHand> POKER_HANDS = Arrays.asList(FiveOfAKindPokerHand.INSTANCE,
+            FourOfAKindPokerHand.INSTANCE, StraightFlushPokerHand.INSTANCE);
+
+    @Override
+    public PokerHand determineBestHand(Set<Card> cards) {
+        LOGGER.info("Determining best poker from card collection: {}",cards);
+        Map<Card.Rank, List<Card.Suit>> rankSuitMap = cards.stream()
+                .collect(Collectors.groupingBy(Card::getRank, Collectors.mapping(Card::getSuit, Collectors.toList())));
+        List<PokerHand> sortedPossiblePokerHands = POKER_HANDS.stream().filter(pokerHand -> pokerHand.matches(rankSuitMap))
+                .sorted(Comparator.comparingInt(PokerHand::getRank)).collect(Collectors.toList());
+        return sortedPossiblePokerHands.get(0);
+    }
+}
