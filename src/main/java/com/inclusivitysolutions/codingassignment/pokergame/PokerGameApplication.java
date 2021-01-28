b12da698ccc73f26b3830db833523b9d09356c2b
Fact S Musingarimi <fmusingarimi@gmail.com>
2021-01-28 11:24:19 +0200

    first commit

---
+++ src/main/java/com/inclusivitysolutions/codingassignment/pokergame/PokerGameApplication.java
@@ -0,0 +1,13 @@
+package com.inclusivitysolutions.codingassignment.pokergame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokerGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokerGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inclusivitysolutions/codingassignment/pokergame/PokerGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inclusivitysolutions.codingassignment.pokergame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokerGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
